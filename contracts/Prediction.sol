1c39d2cf30784c29a6c3334d45fbfd14317b335d
RetepMil <lkslyj2@naver.com>
2022-02-28 17:56:09 +0900

    refactor: 변수명 직관적으로 변경, 코딩 컨벤션 일관성 유지하도록 변경

--- contracts/Prediction.sol
+++ contracts/Prediction.sol
@@ -9,35 +9,41 @@ contract Prediction {
         Heo
     }
 
+    address public oracle;
+    uint256 public commissionPercentage;
+    uint256 public electionStartDate;
+
     Side public result;
-    
+    bool public resultReported;
+
     mapping(Side => uint256) public bets;
     mapping(address => mapping(Side => uint256)) public betsPerGambler;
 
-    address public oracle;
-    uint256 public commissionPercentage;
-    uint256 public placeBetEndDate;
-    bool public electionFinished;
-
-    constructor(address _oracle, uint256 _commissionPercentage, uint256 _placeBetEndDate) {
+    constructor(
+        address _oracle,
+        uint256 _commissionPercentage,
+        uint256 _electionStartDate
+    ) {
         oracle = _oracle;
         commissionPercentage = _commissionPercentage;
-        placeBetEndDate = _placeBetEndDate;
+        electionStartDate = _electionStartDate;
     }
 
     function placeBet(Side _side) external payable {
-        require(electionFinished == false, "election is finished");
-        require(placeBetEndDate>block.timestamp, "placebet is finished");
+        require(resultReported == false, "result is already reported");
+        require(
+            electionStartDate > block.timestamp,
+            "election has already started"
+        );
+
         bets[_side] += msg.value;
         betsPerGambler[msg.sender][_side] += msg.value;
     }
 
-    
-
     function withdrawGain() external {
-        require(electionFinished == true, "election not finished");
-
+        require(resultReported == true, "result is not reported yet");
         uint256 gamblerBet = betsPerGambler[msg.sender][result];
+
         require(
             gamblerBet > 0,
             "you do not have any winning bet, or you already have received the gain"
@@ -50,10 +56,9 @@ contract Prediction {
             bets[Side.Shim] +
             bets[Side.Heo];
         uint256 totalWinnerPrize = bets[result];
-        uint256 gain = (totalPrize / totalWinnerPrize) *
+        uint256 gain = ((totalPrize / totalWinnerPrize) *
             gamblerBet *
-            (100 - commissionPercentage) /
-            100;
+            (100 - commissionPercentage)) / 100;
 
         // prevent getting gain twice
         betsPerGambler[msg.sender][Side.Lee] = 0;
@@ -66,19 +71,26 @@ contract Prediction {
     }
 
     function reportResult(Side _winner) external {
-        require(oracle == msg.sender, "only oracle");
-        require(electionFinished == false, "election is finished");
-        require(placeBetEndDate < block.timestamp, "placebet is not finished yet");
-        result = _winner;
-        electionFinished = true;
+        require(
+            oracle == msg.sender,
+            "only oracle is allowed to report result"
+        );
+        require(resultReported == false, "election result is already reported");
+        require(
+            electionStartDate < block.timestamp,
+            "placebet is not finished yet"
+        );
 
-        uint256 oracle_gain = (bets[Side.Lee] + bets[Side.Yoon] + bets[Side.Ahn] + bets[Side.Shim] + bets[Side.Heo]) *
-                commissionPercentage/ 
-                100;
+        result = _winner;
+        resultReported = true;
+
+        uint256 oracle_gain = ((bets[Side.Lee] +
+            bets[Side.Yoon] +
+            bets[Side.Ahn] +
+            bets[Side.Shim] +
+            bets[Side.Heo]) * commissionPercentage) / 100;
 
         // send oracle for profit
-        msg.sender.transfer(
-            oracle_gain
-        );
+        msg.sender.transfer(oracle_gain);
     }
 }
